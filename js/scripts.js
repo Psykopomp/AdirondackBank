c4c35ea79f3b6b57ef49353459d8b6b4a2f64d1d
Jake Rod <jakelearnscode@gmail.com>
2021-07-07 10:40:45 -0500

    beautiful bars

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+        $('.responsive').slick({
+            dots: true,
+            arrows: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true,
+                        arrows: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    });
